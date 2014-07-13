418c46e460675927448035839ea24f66bcd3723c
yzzheng <yzzheng316@gmail.com>
2014-07-13 16:42:15 -0400

    This is my Exploratory Data Analysis Course Project 1

--- plot3.R
+++ plot3.R
@@ -11,6 +11,6 @@ plot (data1$datetime, data1$Sub_metering_1, xlab ="", ylab = "Energy sub meterin
 ## add additional lines
 lines (data1$datetime, data1$Sub_metering_2, col ="red")
 lines (data1$datetime, data1$Sub_metering_3, col ="blue")
-legend("topright", lty= c(1,1,1), lwd = c(2,2,2), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", lty= c(1,1,1), lwd = c(1,1,1), pch =21, cex =0.5, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 dev.copy(png, file = "plot3.png") ## Copy my plot to a PNG file
 dev.off()
