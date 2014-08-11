cfcb15f494f7a7038d7cc6bec76f9b97f9acbed7
yzzheng <yzzheng316@gmail.com>
2014-08-10 20:40:12 -0400

    Exploratory Data Analysis Course Project1 2nd version

--- plot3.R
+++ plot3.R
@@ -13,6 +13,6 @@ with(data1, plot(datetime, Sub_metering_1, xlab="", ylab="Energy sub metering", 
 ##plot(data1, lines(datetime, Sub_metering_1))
 with(data1, lines(datetime, Sub_metering_2, col="red"))
 with(data1, lines(datetime, Sub_metering_3, col="blue"))
-legend("topright", lty= c(1,1,1), pch=20, cex=0.75, lwd = c(1,1,1), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", lty= c(1,1,1), pch=20, cex=0.65, lwd = c(1,1,1), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 dev.copy(png, file="plot3.png")  ## Copy my plot to a PNG file
 dev.off()
